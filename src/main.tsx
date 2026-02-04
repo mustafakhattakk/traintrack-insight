e935bf2953886d1d1c52117c47c57a1c6644ff79
Mustafa Khattak <mustafakhattakk7@gmail.com>
2026-02-04 15:41:44 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../App";
-import "./index.css"; // only if you actually have Tailwind/CSS set up
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
