193a06249f9852e613bdccb186bfc398225f2085
Mustafa Khattak <mustafakhattakk7@gmail.com>
2026-02-04 16:24:45 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
+import Dashboard from "./components/Dashboard";
+
 export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>🚆 TrainTrack Insight</h1>
-      <p>App is working.</p>
-    </div>
-  );
+  return <Dashboard />;
 }
